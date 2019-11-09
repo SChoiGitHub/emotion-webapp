3fc456ec46b5816717c11c720763d30bf234ee4c
a <a@10-0-2-15-static.hitronhub.home>
2019-11-08 22:56:06 -0600

    backend can now connect with vokaturi service (coupled

--- backend/backend.js
+++ backend/backend.js
@@ -1,6 +1,19 @@
 const express = require('express')
 var path = require('path');
 const app = express()
+const axios = require('axios')
+
+function getEmotionData(filename){
+  return new Promise((resolve,reject) => {
+    axios.get("http://localhost:3000/vokaturi/"+filename)
+    .then(response => {
+      resolve(response)
+    })
+    .catch(() => {
+      reject("error")
+    })
+  }) 
+}
 
 
 app.set('view engine', 'ejs');
