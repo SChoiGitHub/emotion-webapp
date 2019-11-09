ba27bbd58c0842476cb85319bd70fd0a7e0df95e
a <a@10-0-2-15-static.hitronhub.home>
2019-11-08 23:09:47 -0600

    gitignore, open_vokaturi refactored to allow paths

--- vokaturi_interface/vokaturi.py
+++ vokaturi_interface/vokaturi.py
@@ -1,28 +1,27 @@
 from flask import Flask, escape, request, jsonify
-from os import environ
+from os import environ, path
 from scipy.io.wavfile import read as wav_read
 import sys
-sys.path.append("vokaturi_interface/OpenVokaturi/api")
+sys.path.append(path.join(environ["OPENVOKATURI_PATH"],"api"))
 import Vokaturi
 
 
 try:
-    Vokaturi.load("vokaturi_interface/OpenVokaturi/lib/open/linux/OpenVokaturi-3-3-linux64.so")
+    Vokaturi.load(path.join(environ["OPENVOKATURI_PATH"],"lib/open/linux/OpenVokaturi-3-3-linux64.so"))
 except:
     sys.exit(0)
 
 app = Flask(__name__)
 
-@app.route('/vokaturi/<string:wav_file>')
+@app.route('/vokaturi/<path:wav_file>')
 def vokaturi(wav_file):
-    (sample_rate, samples) = wav_read("vokaturi_interface/"+wav_file)
+    (sample_rate, samples) = wav_read(wav_file)
     buffer_length = len(samples)
     c_buffer = Vokaturi.SampleArrayC(buffer_length)
-    if samples.ndim == 1:  # mono
+    if samples.ndim == 1:
         c_buffer[:] = samples[:] / 32768.0
-    else:  # stereo
+    else:
         c_buffer[:] = 0.5*(samples[:,0]+0.0+samples[:,1]) / 32768.0
-    
     voice = Vokaturi.Voice (sample_rate, buffer_length)
     voice.fill(buffer_length, c_buffer)
     quality = Vokaturi.Quality()
