999d59ec70bda62c3538d6bea95678b20684e2f6
poudel <poudel@AGR-1211-Poudel.local>
2019-11-08 23:38:31 -0600

    add code to record audio from microphone

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 var path = require('path');
+
+const recorder = require('node-record-lpcm16')
+const fs = require('fs')
+
+
+
+
+
 const app = express()
 
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded())
+
 
 app.get('/', function (req, res) {
+    
+
+    
     res.render('index')
   })
 
@@ -14,5 +27,19 @@ app.get('*', function(req, res) {
      res.render('error');
     });
 
+app.post("/speak", function(req,res){
+        console.log('speak reached');
+
+        const file = fs.createWriteStream('test.wav', { encoding: 'binary' })
+ 
+        recorder.record({
+        sampleRate: 44100
+        })
+        .stream()
+        .pipe(file)
+        
+        res.redirect('/');
+    });
+
    
-  app.listen(8080)
+  app.listen(9010)
